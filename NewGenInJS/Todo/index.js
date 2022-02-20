84fec9e6e71ad751dab91b092a31fa94c5f4a2e0
Rashad <87971037+RashadM0@users.noreply.github.com>
2022-02-20 16:41:47 +0400

    Update1.1

--- NewGenInJS/Todo/index.js
+++ NewGenInJS/Todo/index.js
@@ -1,23 +1,27 @@
 const form = document.querySelector("#todo-form");
 const input = document.querySelector("#todo");
 const ul = document.querySelector(".list-group");
-const firstBody = document.querySelector(".card-body")[1];
-const secondBody = document.querySelector(".card-body")[2];
+const firstBody = document.querySelectorAll(".card-body")[0];
+const secondBody = document.querySelectorAll(".card-body")[1];
 const filter = document.querySelector("#filter");
 const clear = document.querySelector("#clear-todos");
 //
-
+//eventListeners
 form.addEventListener("submit", addTodo);
 
-//
+//create todo
 function addTodo(e) {
   e.preventDefault();
   const todo = input.value.trim();
-  addUI(todo);
+  if (todo === "") {
+    showAlert("danger", "Add any todo!");
+  } else {
+    addUI(todo);
+    showAlert("success", "Todo added successfully!")
+  }
 }
 
-//
-
+//Add todo to ui
 function addUI(todo) {
   const li = document.createElement("li");
   li.className = "list-group-item d-flex justify-content-between";
@@ -27,4 +31,17 @@ function addUI(todo) {
   li.appendChild(document.createTextNode(todo));
   li.appendChild(link);
   ul.appendChild(li);
+  input.value = "";
 }
+
+//create alert messages
+function showAlert(type, message){
+  const alert = document.createElement("div");
+  alert.className = `alert alert-${type}`;
+  alert.textContent = message;
+  firstBody.appendChild(alert)
+  setTimeout(function (){
+    alert.remove();
+  },1000);
+}
+
