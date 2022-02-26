e77a40bc6cc786e283e9141bb6f475ac32c21d00
Rashad <87971037+RashadM0@users.noreply.github.com>
2022-02-26 19:21:46 +0400

    hello

--- NewGenInJS/ES6/index.js
+++ NewGenInJS/ES6/index.js
@@ -18,8 +18,14 @@
 
 //destrucing
 
-let number1, number2;
-arr = [1, 2, 3, 4, 5];
-number1 = arr[0];
-number2 = arr[1];
-console.log(number1, number2);
+// let number1, number2;
+// arr = [10, 20, 30, 40, 50];
+// number1 = arr[0];
+// number2 = arr[1];
+// const [number1, number2] = arr;
+
+// console.log(number1, number2);
+
+// const langs = () => ["Java", "JS", "C++"]
+// const [lang1, lang2, lang3]= langs()
+// console.log(lang2, lang1, lang3);
